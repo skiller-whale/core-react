3757fe2b2581481b2ce2523d066c483215db2abc
Amyas Merivale <merivale@gmail.com>
2025-09-09 15:16:16 +0200

    Get HMR working in hosted environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { Server } from "node:http";
+import { defineConfig } from "vite";
 
 const hostedEnvHost = process.env.SW_HOSTNAME_PORT_3500;
+const hostedEnvHotReloadHost = process.env.SW_HOSTNAME_PORT_3501;
 
 export default defineConfig({
   server: {
     allowedHosts: hostedEnvHost ? [hostedEnvHost] : undefined,
-    hmr: {
-      port: 3501,
-    },
+    hmr: hostedEnvHotReloadHost
+      ? {
+          clientPort: 443,
+          host: hostedEnvHotReloadHost,
+          server: new Server().listen({
+            port: 3501,
+            hostname: hostedEnvHotReloadHost,
+          }),
+        }
+      : {
+          port: 3501,
+        },
   },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       lib: "/lib",
       complete: "/_complete",
-      "src_js": "/src_js",
-      "src_ts": "/src_ts",
+      src_js: "/src_js",
+      src_ts: "/src_ts",
     },
   },
   test: {
